60c8362c692a527906e23b23147d57f96457913d
Brueni <info@michael-bruenisholz.ch>
2017-03-19 13:10:45 +0100

    Bugfixes

--- set_nrdylight.php
+++ set_nrdylight.php
@@ -1,4 +1,4 @@
 <?php
-$script = "/var/homecontrol-backend/set_nrdylight.sh " . $_GET['mode'];
+$script = "/var/homecontrol-backend/set_nrdylight.sh " . $_GET['mode'] . " > /dev/null";
 shell_exec($script);
 ?>
--- set_nrdylight.sh
+++ set_nrdylight.sh
@@ -3,10 +3,16 @@ cd `dirname $0`
 
 actionfile="/var/nrdylights/actions/next.action"
 
-if [ "$1" = "on" ]
-then
+if [ "$1" = "on" ]; then
 	sed -i '1s/.*/static/' $actionfile
 	sudo python /var/nrdylights/light.py &
+elif [ "$1" = "toggle" ]; then
+	if [ -f /var/nrdylights/light.lock ]; then #Check if Lights are running trough lockfile
+		sed -i '1s/.*/exit/' $actionfile
+	else
+		sed -i '1s/.*/static/' $actionfile
+	        sudo python /var/nrdylights/light.py &
+	fi
 else
 	sed -i '1s/.*/exit/' $actionfile
 fi
