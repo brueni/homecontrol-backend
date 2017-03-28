b423fddf8267ba6dbe59aea80b237132f6c78882
Michael Brünisholz <info@michael-bruenisholz.ch>
2017-03-28 18:21:32 +0200

    Added the Switch device on/off file

---
+++ dev_power.php
@@ -0,0 +1,55 @@
+<?php
+
+function wol($broadcast, $mac)
+{
+    $mac_array = split(':', $mac);
+
+    $hwaddr = '';
+
+    foreach($mac_array AS $octet)
+    {
+        $hwaddr .= chr(hexdec($octet));
+    }
+
+    // Create Magic Packet
+
+    $packet = '';
+    for ($i = 1; $i <= 6; $i++)
+    {
+        $packet .= chr(255);
+    }
+
+    for ($i = 1; $i <= 16; $i++)
+    {
+        $packet .= $hwaddr;
+    }
+
+    $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+    if ($sock)
+    {
+        $options = socket_set_option($sock, 1, 6, true);
+
+        if ($options >=0)
+        {
+            $e = socket_sendto($sock, $packet, strlen($packet), 0, $broadcast, 7);
+            socket_close($sock);
+        }
+    }
+}
+
+////// Broadcast-Adresse
+$bc = "192.168.11.255";
+
+////// Mögliche Geräte
+$devices = array ( 'nas3270' => '00:1c:c0:e4:ee:14',
+                 'srv1' => '00:1e:8c:e4:bc:8f',
+                 'htpc' => '00:30:1b:44:73:00' );
+
+if ($_GET['action'] == 'on') {
+  $hw = $devices[$_GET['dev']];
+  wol($bc, $hw);
+elseif ($_GET['action'] == 'off') {
+  
+}
+
+?>
