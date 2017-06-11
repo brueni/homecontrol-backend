59747cc8cbbe5af851b8981da4a2e2028013cce7
Michael Brünisholz <info@michael-bruenisholz.ch>
2017-06-11 15:55:27 +0200

    VLC Controlscript

---
+++ ctrl_vlc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+vlc_host = "htpc.home:8080";
+vlc_auth = ":1"; #empty User, '1' as password
+
+vlc_cmd = $1
+vlc_arg1 = $2
+
+curl -s -u $vlc_auth http://${vlc_host}/requests/status.xml?command=${vlc_cmd}${vlc_arg1} > /dev/null
